3e132b35444fce52bdbb1a1fe32bfa6d0cd99056
Anowarul Kabir <akabir0101@gmail.com>
2023-06-10 22:25:58 -0400

    sequnet run and refactored

--- models/sequnet_dunham/deprecated/popu_freq_pred.py
+++ models/sequnet_dunham/deprecated/popu_freq_pred.py
@@ -35,24 +35,24 @@ if __name__=="__main__":
 
     chunk_size = 1
     data_chunks = [data[x:x+chunk_size] for x in range(0, len(data), chunk_size)]
-    # data_chunks = data_chunks[:10] 
+    data_chunks = data_chunks[:20] 
     print(f"#-of chunks: {len(data_chunks)}, 1st chunk size: {len(data_chunks[0])}")
 
     
     pred_dfs = []
     # sequential run
-    for i, data_chunk in enumerate(data_chunks):
-        pred_df = execute(data_chunk)
-        print(f"Finished {i}/{len(data_chunks)}th chunk: {pred_df.shape}")
-        pred_dfs.append(pred_df)
-
-    # mpi run    
-    # from mpi4py.futures import MPIPoolExecutor
-    # executor = MPIPoolExecutor()
-    # for i, pred_df in enumerate(executor.map(execute, data_chunks, unordered=True)):
+    # for i, data_chunk in enumerate(data_chunks):
+    #     pred_df = execute(data_chunk)
     #     print(f"Finished {i}/{len(data_chunks)}th chunk: {pred_df.shape}")
     #     pred_dfs.append(pred_df)
-    # executor.shutdown()
+
+    # mpi run    
+    from mpi4py.futures import MPIPoolExecutor
+    executor = MPIPoolExecutor()
+    for i, pred_df in enumerate(executor.map(execute, data_chunks, unordered=True)):
+        print(f"Finished {i}/{len(data_chunks)}th chunk: {pred_df.shape}")
+        pred_dfs.append(pred_df)
+    executor.shutdown()
 
     result_df = pd.concat(pred_dfs)
     print("Saving predictions ...")  
--- models/sequnet_dunham/pred_nonmasked.py
+++ models/sequnet_dunham/pred_nonmasked.py
@@ -5,12 +5,15 @@ sys.path.append("../variant_effect_analysis")
 import time
 import pandas as pd
 
-# from models.aa_common.data_loader import get_protein_sequences, get_pmd_dataset
-from models.aa_common.data_loader import get_pmd_dbnsfp_dataset
+from models.aa_common.data_loader import get_pmd_dbnsfp_dataset, get_popu_freq_dbnsfp_dataset, get_patho_likelypatho_neutral_dbnsfp_dataset
 import models.sequnet_dunham.model_utils as model_utils
 
-task = "pmd"
-variants_df, seq_record_list = get_pmd_dbnsfp_dataset(home_dir, seq_return_type="seq_record_list")
+
+task = "patho" # pmd, popu_freq, patho
+# variants_df, seq_record_list = get_pmd_dbnsfp_dataset(home_dir, seq_return_type="seq_record_list")
+# variants_df, seq_record_list = get_popu_freq_dbnsfp_dataset(home_dir, seq_return_type="seq_record_list")
+variants_df, seq_record_list = get_patho_likelypatho_neutral_dbnsfp_dataset(home_dir, seq_return_type="seq_record_list")
+
 
 model_name = "sequnet"
 model = model_utils.get_model()
@@ -36,29 +39,28 @@ if __name__ == "__main__":
 
     chunk_size = 1 # 32 if torch.cuda.is_available() else 1
     data_chunks = [data[x:x+chunk_size] for x in range(0, len(data), chunk_size)]
-    # data_chunks = data_chunks[:10] 
+    # data_chunks = data_chunks[:20] 
     print(f"#-of chunks: {len(data_chunks)}, 1st chunk size: {len(data_chunks[0])}")
     
     pred_dfs = []
     # sequential run and debugging
-    for i, data_chunk in enumerate(data_chunks):
-        pred_df = execute(data_chunk)
-        print(f"Finished {i}/{len(data_chunks)}th chunk: {pred_df.shape}")
-        pred_dfs.append(pred_df)
-
-    # mpi run    
-    # from mpi4py.futures import MPIPoolExecutor
-    # executor = MPIPoolExecutor()
-    # for i, pred_df in enumerate(executor.map(execute, data_chunks, unordered=True)):
+    # for i, data_chunk in enumerate(data_chunks):
+    #     pred_df = execute(data_chunk)
     #     print(f"Finished {i}/{len(data_chunks)}th chunk: {pred_df.shape}")
     #     pred_dfs.append(pred_df)
-    # executor.shutdown()
+
+    # mpi run    
+    from mpi4py.futures import MPIPoolExecutor
+    executor = MPIPoolExecutor()
+    for i, pred_df in enumerate(executor.map(execute, data_chunks, unordered=True)):
+        print(f"Finished {i}/{len(data_chunks)}th chunk: {pred_df.shape}")
+        pred_dfs.append(pred_df)
+    executor.shutdown()
     
     result_df = pd.concat(pred_dfs)  
     print("Saving predictions ...")
     result_df.to_csv(f"{model_task_out_dir}/preds_{model_name}_masked.tsv", sep="\t", index=False, header=True)
     print(result_df.shape)
     print(result_df.head())
-
         
     print(f"Time taken: {time.time()-start} seconds")
