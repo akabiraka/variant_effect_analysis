67bed80ec0a055fc5d43edef2bc3523bed5d7c51
Anowarul Kabir <akabir0101@gmail.com>
2023-06-10 22:02:02 -0400

    prot-bert-bfd configured for patho

--- models/bioembeddings_dallago/pred_masked.py
+++ models/bioembeddings_dallago/pred_masked.py
@@ -5,15 +5,19 @@ home_dir = ""
 import time
 import  pandas as pd
 
-from models.aa_common.data_loader import get_pmd_dbnsfp_dataset
+from models.aa_common.data_loader import get_pmd_dbnsfp_dataset, get_popu_freq_dbnsfp_dataset, get_patho_likelypatho_neutral_dbnsfp_dataset
 import models.bioembeddings_dallago.model_utils as model_utils
 
-task = "pmd"
-variants_df, protid_seq_dict = get_pmd_dbnsfp_dataset(home_dir)
+task = "patho" # pmd, popu_freq, patho
+# variants_df, protid_seq_dict = get_pmd_dbnsfp_dataset(home_dir)
+# variants_df, protid_seq_dict = get_popu_freq_dbnsfp_dataset(home_dir)
+variants_df, protid_seq_dict = get_patho_likelypatho_neutral_dbnsfp_dataset(home_dir)
+
 
 model_name = "prottrans_bert_bfd" 
 model, tokenizer, model_name = model_utils.get_model_tokenizer(model_name)
 model_task_out_dir, model_logits_out_dir = model_utils.create_output_directories(model_name, task)
+
 
 def execute(protid_mutpos_tuple_list):
     preds = []        
@@ -26,6 +30,7 @@ def execute(protid_mutpos_tuple_list):
     preds_df = pd.DataFrame(preds)   
     return preds_df
 
+
 if __name__ == "__main__":
     start = time.time()
 
@@ -34,7 +39,7 @@ if __name__ == "__main__":
 
     chunk_size = 1 # 32 if torch.cuda.is_available() else 1
     data_chunks = [data[x:x+chunk_size] for x in range(0, len(data), chunk_size)]
-    # data_chunks = data_chunks[:10] 
+    # data_chunks = data_chunks[:20] 
     print(f"#-of chunks: {len(data_chunks)}, 1st chunk size: {len(data_chunks[0])}")
 
     pred_dfs = []
