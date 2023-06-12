691ab6dd797d6cfaff4c07475b35f848860e2944
Anowarul Kabir <akabir0101@gmail.com>
2023-06-12 11:06:16 -0400

    plots updated

---
+++ models/aa_common/plots_helper.py
@@ -0,0 +1,9 @@
+
+
+pmd_colors_dict = {"Effect":"darkred", "Knock-out":"red", "No-effect":"darkblue"}
+patho_colors_dict = {"Pathogenic":"coral", "Likely-pathogenic":"lightcoral", "Neutral":"gray"}
+popu_freq_colors_dict = {"Singleton":"maroon", "Ultra-rare":"chocolate", "Rare":"orange", "Common":"lightsteelblue"}
+
+pmd_class_order = ["Effect", "Knock-out", "No-effect"]
+patho_class_order = ["Pathogenic", "Likely-pathogenic", "Neutral"]
+popu_freq_class_order = ["Singleton", "Ultra-rare", "Rare", "Common"]
